4af44ee158bfd3138b62bea5227fecf9ed43cd00
Amin Espinoza <aminespinoza@CompuAmin>
2023-09-24 12:05:32 -0600

    new test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,13 +16,16 @@ resource "azurerm_service_plan" "service_plan" {
 }
 
 resource "azurerm_linux_web_app" "example" {
-  name                = "coffeshop"
+  name                = "aminescoffeshop"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.rg.location
+  location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.service_plan.id
 
   site_config {
     always_on = true
-    linux_fx_version = "DOCKER|${var.container_image}"
+    application_stack {
+      docker_image_name = var.container_image
+      docker_registry_url = "https://index.docker.io"
+    }
   }
 }

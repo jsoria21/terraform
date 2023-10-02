9e086983fdff7e576de6818b67af5eb37f4c6429
Amin Espinoza <aminespinoza@CompuAmin>
2023-10-01 19:56:00 -0600

    backend added

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     storage_account_name = "generalstorageamin"
-    container_name       = "terraform"
+    container_name       = "webappstate"
     key                  = "stateActions.tfstate"
   }
 }

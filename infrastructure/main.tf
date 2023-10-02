c7ae4754a2037e79c5f9ff5115d2d4276f7c36ff
Amin Espinoza <aminespinoza@CompuAmin>
2023-10-01 21:14:12 -0600

    new features added to infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,3 +8,10 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_service_plan" "service_plan" {
+  name                = "webapp-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}

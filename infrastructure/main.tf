318682e39b6166b829eb97bce6fbb4986843c2a3
Amin Espinoza <aminespinoza@CompuAmin>
2023-10-02 13:05:10 -0600

    terraform ready

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,11 +7,3 @@ resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
 }
-
-resource "azurerm_service_plan" "service_plan" {
-  name                = "webapp-plan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
-}

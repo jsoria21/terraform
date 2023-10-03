20ca4260b8d973457604bed86154fdb5f3cc0114
Amin Espinoza <aminespinoza@CompuAmin>
2023-10-03 12:32:37 -0600

    new infra to deploy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,3 +7,26 @@ resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
 }
+
+resource "azurerm_service_plan" "service_plan" {
+  name                = "webapp-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "aminescoffeshop"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.service_plan.id
+
+  site_config {
+    always_on = true
+    application_stack {
+      docker_image_name = var.container_image
+      docker_registry_url = "https://index.docker.io"
+    }
+  }
+}
